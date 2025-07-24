99a4bdf8b117dd232781a646cae211c55416d33f
Iris <iridian95@live.com>
2025-07-23 22:15:18 -0400

    added movie details endpoint

--- src/main/java/com/iridian/movie/social/repository/UserRepository.java
+++ src/main/java/com/iridian/movie/social/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.iridian.movie.social.model.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
 }
