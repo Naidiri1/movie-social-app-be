95014e15728262030f5511ba40ab89d8c8b4ea92
Iris <iridian95@live.com>
2025-08-11 20:03:58 -0400

    removed hardcoded time fromto dates and filtering upcoming movies

--- src/main/java/com/iridian/movie/social/service/TMDBService.java
+++ src/main/java/com/iridian/movie/social/service/TMDBService.java
@@ -1,6 +1,8 @@
 package com.iridian.movie.social.service;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -12,11 +14,10 @@ public class TMDBService {
     private final WebClient webClient;
 
     public TMDBService(WebClient.Builder webClientBuilder,
-                       @Value("${tmdb.api.url}") String apiUrl,
-                       @Value("${tmdb.api.token}") String apiToken) {
+            @Value("${tmdb.api.url}") String apiUrl,
+            @Value("${tmdb.api.token}") String apiToken) {
 
-       
-                        this.webClient = webClientBuilder
+        this.webClient = webClientBuilder
                 .baseUrl(apiUrl)
                 .defaultHeader("Authorization", "Bearer " + apiToken)
                 .build();
@@ -25,53 +26,55 @@ public class TMDBService {
     public String searchMovies(String id) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .path("/search/movie")
-                        .queryParam("query", id)
-                        .build())
+                .path("/search/movie")
+                .queryParam("query", id)
+                .build())
                 .retrieve()
                 .bodyToMono(String.class)
-                .block(); 
+                .block();
     }
 
     public String getPopularMovies() {
-    return webClient.get()
-            .uri("/movie/popular")
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
+        return webClient.get()
+                .uri("/movie/popular")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 
-   public String getUpcomingMovies() {
-    LocalDate startDate = LocalDate.of(2025, 8, 11);
-    LocalDate endDate = startDate.plusMonths(1).withDayOfMonth(startDate.plusMonths(1).lengthOfMonth());
+    public String getUpcomingMovies() {
+        ZoneId zone = ZoneId.of("America/New_York");
+        LocalDate startDate = LocalDate.now(zone);
+        LocalDate endDate = startDate.plusMonths(1).with(TemporalAdjusters.lastDayOfMonth());
 
-    return webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                      .path("/discover/movie")
-                    .queryParam("include_adult", "false")
-                    .queryParam("include_video", "false")
-                    .queryParam("language", "en-US")
-                    .queryParam("page", "1")
-                    .queryParam("sort_by", "popularity.desc")
-                    .queryParam("with_release_type", "2|3") 
-                    .queryParam("primary_release_date.gte", startDate.toString())
-                    .queryParam("primary_release_date.lte", endDate.toString())
-                    .build()
-            )
-            .retrieve()
-            .bodyToMono(String.class)
-            .block();
-   }
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                .path("/discover/movie")
+                .queryParam("include_adult", "false")
+                .queryParam("include_video", "false")
+                .queryParam("language", "en-US")
+                .queryParam("page", "1")
+                 .queryParam("certification_country", "US")
+                .queryParam("sort_by", "popularity.desc")
+                .queryParam("certification.lte", "PG-13")
+                .queryParam("primary_release_date.gte", startDate.toString())
+                .queryParam("primary_release_date.lte", endDate.toString())
+                .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
 
     public String getMovieDetails(Number id) {
         return webClient.get()
                 .uri(uriBuilder -> uriBuilder
-                        .path("/movie/{movie_id}")
-                        .queryParam("id", id)
-                        .build(id))
+                .path("/movie/{movie_id}")
+                .queryParam("id", id)
+                .build(id))
                 .retrieve()
                 .bodyToMono(String.class)
-                .block(); 
+                .block();
     }
 
 }
